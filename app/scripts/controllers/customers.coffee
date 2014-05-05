489617f5d643e62b2fb322bb7bf246d8d39e0353
Wang Hao <eric.3126@hotmail.com>
2014-05-05 17:06:49 +0800

    Update: `fnDrawCallback` && $compile($(element))($scope)
    1. using `fnDrawCallback` as callback option to initialize some element
    2. using $compile($(element))($scope) to tell Angular to scope the new
    element dynamically.

--- app/scripts/controllers/customers.coffee
+++ app/scripts/controllers/customers.coffee
@@ -10,22 +10,33 @@ app.controller 'RoleCtrl', ($scope, DTOptionsBuilder, DTColumnBuilder) ->
 	]
 	return
 
-app.controller 'PartnerCtrl', ($scope, DTOptionsBuilder, DTColumnBuilder) ->
+app.controller 'PartnerCtrl', ($scope, $compile, DTOptionsBuilder, DTColumnBuilder) ->
+
+	$scope.click_test = ->
+		alert 'hello world'
+		console.log 'hello world'
+	$scope.test = 'test123'
 	$scope.dtOptions = DTOptionsBuilder.fromSource('/api/customers/partners')
+		.withOption('fnDrawCallback', ->
+			$compile($('.blue'))($scope)
+		)
 		.withBootstrap()
+
 	$scope.dtColumns = [
-		DTColumnBuilder.newColumn('name').withTitle('合作商名').renderWith((data)-> "<a href='www.baidu.com'>#{data}</a>"),
+		DTColumnBuilder.newColumn('name').withTitle('合作商名').renderWith( (data)-> 
+			"<a href='javascript:void(0)' role='button' class='blue' data-toggle='modal' data-target='#modal-form'> #{data} </a>"),
 		DTColumnBuilder.newColumn('email').withTitle('邮箱'),
 		DTColumnBuilder.newColumn('telephone').withTitle('联系电话'),
 		DTColumnBuilder.newColumn('address').withTitle('地址'),
 		DTColumnBuilder.newColumn('identifier').withTitle('标识'),
 		DTColumnBuilder.newColumn('products').withTitle('订购产品').renderWith((data) -> data.join(' ')),
 		DTColumnBuilder.newColumn('createTime').withTitle('创建时间'),
-		DTColumnBuilder.newColumn('id')
-					   .withTitle('操作')
-					   .renderWith((data, type, full) ->
-					   		console.log data
-					   		return data + '11111'
-					   )
+		DTColumnBuilder.newColumn('id').withTitle('操作').renderWith((data, type, full) ->
+				return 	"<div class='visible-md visible-lg hidden-sm hidden-xs action-buttons'>
+							<a class='blue' ng-click='click_test()'><i class='icon-zoom-in bigger-130'></i></a>
+							<a class='green' href='javascript:void(0)'><i class='icon-pencil bigger-130'></i></a>
+							<a class='red' href='javascript:void(0)'><i class='icon-trash bigger-130'></i></a>
+						</div>")
 	]
+	
 	return
--- app/scripts/controllers/customers.js
+++ app/scripts/controllers/customers.js
@@ -5,16 +5,22 @@ app.controller('RoleCtrl', function($scope, DTOptionsBuilder, DTColumnBuilder) {
   $scope.dtColumns = [DTColumnBuilder.newColumn('role').withTitle('角色'), DTColumnBuilder.newColumn('create_time').withTitle('创建时间'), DTColumnBuilder.newColumn('operation').withTitle('操作')];
 });
 
-app.controller('PartnerCtrl', function($scope, DTOptionsBuilder, DTColumnBuilder) {
-  $scope.dtOptions = DTOptionsBuilder.fromSource('/api/customers/partners').withBootstrap();
+app.controller('PartnerCtrl', function($scope, $compile, DTOptionsBuilder, DTColumnBuilder) {
+  $scope.click_test = function() {
+    alert('hello world');
+    return console.log('hello world');
+  };
+  $scope.test = 'test123';
+  $scope.dtOptions = DTOptionsBuilder.fromSource('/api/customers/partners').withOption('fnDrawCallback', function() {
+    return $compile($('.blue'))($scope);
+  }).withBootstrap();
   $scope.dtColumns = [
     DTColumnBuilder.newColumn('name').withTitle('合作商名').renderWith(function(data) {
-      return "<a href='www.baidu.com'>" + data + "</a>";
+      return "<a href='javascript:void(0)' role='button' class='blue' data-toggle='modal' data-target='#modal-form'> " + data + " </a>";
     }), DTColumnBuilder.newColumn('email').withTitle('邮箱'), DTColumnBuilder.newColumn('telephone').withTitle('联系电话'), DTColumnBuilder.newColumn('address').withTitle('地址'), DTColumnBuilder.newColumn('identifier').withTitle('标识'), DTColumnBuilder.newColumn('products').withTitle('订购产品').renderWith(function(data) {
       return data.join(' ');
     }), DTColumnBuilder.newColumn('createTime').withTitle('创建时间'), DTColumnBuilder.newColumn('id').withTitle('操作').renderWith(function(data, type, full) {
-      console.log(data);
-      return data + '11111';
+      return "<div class='visible-md visible-lg hidden-sm hidden-xs action-buttons'> <a class='blue' ng-click='click_test()'><i class='icon-zoom-in bigger-130'></i></a> <a class='green' href='javascript:void(0)'><i class='icon-pencil bigger-130'></i></a> <a class='red' href='javascript:void(0)'><i class='icon-trash bigger-130'></i></a> </div>";
     })
   ];
 });
